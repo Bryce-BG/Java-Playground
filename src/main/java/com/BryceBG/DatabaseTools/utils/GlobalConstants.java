9b712731eb502ffa1a94b3573fd3dadc75f7b52e
Bryce-BG <bbodleygomes@umass.edu>
2020-10-28 15:42:56 -0400

    added app name to the global constants

--- src/main/java/com/BryceBG/DatabaseTools/utils/GlobalConstants.java
+++ src/main/java/com/BryceBG/DatabaseTools/utils/GlobalConstants.java
@@ -11,5 +11,6 @@ public class GlobalConstants {
 	public static final String TEST_LOGGER_PATTERN = "%d %p %c [%t] function: %M| %m%n";
 	//basic filename used to output logger results when runing tests (though overflows will result in a date being appended to the file name to uniquely identify.
 	public static final String TEST_LOGGER_OUT_FILE_NAME = "test_log.txt";
-
+	
+	public static final String APP_NAME = "Ebook Library System";
 }
